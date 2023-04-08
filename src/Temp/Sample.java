40c1f9299539541f57dd6227dfc9a39529c4a19d
iAmMujeeb <94049888+iAmMujeeb@users.noreply.github.com>
2023-04-08 13:19:15 +0530

    Delete src/Temp directory

--- src/Temp/Sample.java
+++
@@ -1,9 +0,0 @@
-//package Temp;
-//
-//public class Sample {
-//    public static void main(String[] args) {
-//        System.out.println(com.bridgelabz.basics.AccessSpecifiers2.defaultVar);
-//        System.out.println(com.bridgelabz.basics.AccessSpecifiers2.defaultVar);
-//
-//    }
-//}
